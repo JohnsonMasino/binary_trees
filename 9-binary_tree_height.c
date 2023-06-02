83d1819ee102b65a4b234e84c2773999ce6790c1
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-01 17:50:19 -0700

    corrected file 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		int left = 0, right = 0;
+
 		if (tree->right)
 			right = 1 + binary_tree_height(tree->right);
 		if (tree->left)
